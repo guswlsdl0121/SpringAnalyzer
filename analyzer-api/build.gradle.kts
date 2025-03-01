7846ab927ab64da7e747d3860c3d58328163e5c8
íguswlsdl0121 <guswlsdl0121@gmail.com>
2025-03-02 04:14:03 +0900

    build: build.gradle을 build.gradle.kts로 변경
    - 타입 안정성과 같은 이유로

---
+++ analyzer-api/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	id("java")
+	id("org.springframework.boot") version "3.4.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.hyunjin"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// 기본 의존성
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// AMQP 의존성
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
+
+	// 롬복 의존성
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	// 테스트 의존성
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.amqp:spring-rabbit-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Test>("test") {
+	useJUnitPlatform()
+}
